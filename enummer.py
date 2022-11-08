85f779be96eb5cbfb10f86a3fd40e4d5b65c4e73
Mike Solomon <mike120982@gmail.com>
2022-11-08 19:19:03 +0200

    Adds enummer

---
+++ enummer.py
@@ -0,0 +1,29 @@
+N="InputType"
+A='"date" ｜ "datetime-local" ｜ "email" ｜ "month" ｜ "number" ｜ "password" ｜ "search" ｜ "tel" ｜ "text" ｜ "time" ｜ "url" ｜ "week"'
+
+def acn(n,d):
+    joined = ''.join([y.capitalize() for y in d.split('-')])
+    return f'{n.lower()}{joined}'
+
+def acMe(n,a):
+    asl = [x for x in a.replace('"',' ').replace('|',' ').replace('｜','').split(' ') if x != '']
+    defs = '\n'.join([f'{acn(n,d)} :: {n}\n{acn(n,d)} = {n} "{d}"' for d in asl])
+    return f'''-- starting {n}
+newtype {n} = {n} String
+un{n} :: {n} -> String
+un{n} ({n} t) = t
+{defs}
+'''
+
+def decls(n,a):
+    asl = [x for x in a.replace('"',' ').replace('|',' ').replace('｜','').split(' ') if x != '']
+    defs = '\n  , '.join([f'{acn(n,d)}' for d in asl])
+    return f''' , {n}
+    , un{n}
+    , {defs}
+'''
+if __name__ == '__main__':
+    o = acMe(N,A)
+    o_ = decls(N,A)
+    print(o)
+    print(o_)
