a2f5ec07e711a76381236e3ce20fe28b7fc94bea
Mike Solomon <mike120982@gmail.com>
2022-11-13 21:47:23 +0200

    Adds new cloud function for play games auth

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,32 @@
 const functions = require("firebase-functions");
 const verifier = require("gamecenter-identity-verifier");
+const {defineSecret} = require("firebase-functions/params");
 const admin = require("firebase-admin");
 const cors = require("cors")({origin: true});
+const {google} = require("googleapis");
+
+const nyaaServerClientId = defineSecret("NYAA_SERVER_CLIENT_ID");
+const nyaaServerClientSecret = defineSecret("NYAA_SERVER_CLIENT_SECRET");
+
 
 admin.initializeApp();
 const auth = admin.auth();
 
 const BUNDLE_ID = "fm.joyride.nyaa";
+
+exports.pgAuth = functions
+    .runWith({secrets: [nyaaServerClientId, nyaaServerClientSecret]})
+    .https.onRequest((req, res) => {
+      return cors(req, res, async () => {
+        const oauth2Client = new google.auth.OAuth2(
+            nyaaServerClientId.value(),
+            nyaaServerClientSecret.value(),
+            "https://oauth2.googleapis.com/token",
+        );
+        const {tokens} = await oauth2Client.getToken(req.body.code);
+        return {tokens};
+      });
+    });
 
 exports.gcAuth = functions.https.onRequest((req, res) => {
   return cors(req, res, async () => {
