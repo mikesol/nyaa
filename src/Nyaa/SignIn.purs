634c36cf4d23d5ed260180b634c10f48ee005a95
Mike Solomon <mike120982@gmail.com>
2022-11-14 17:26:42 +0200

    Adds alert if needed

--- src/Nyaa/SignIn.purs
+++ src/Nyaa/SignIn.purs
@@ -3,19 +3,26 @@ module Nyaa.SignIn where
 import Prelude
 
 import Control.Promise (toAffE)
+import Control.Alt ((<|>))
+import Data.Maybe (Maybe(..))
 import Effect.Aff (Aff, bracket)
 import Effect.Class (liftEffect)
 import Nyaa.Capacitor.Utils (Platform(..), getPlatform)
 import Nyaa.Firebase.Firebase (signInWithGoogle, signInWithGameCenter, signInWithPlayGames, signOut)
+import Nyaa.Ionic.Alert (alert)
 import Nyaa.Ionic.Loading (dismissLoading, presentLoading)
 
 signInFlow :: Aff Unit
-signInFlow = bracket (toAffE $ presentLoading "Signing in...") (toAffE <<< dismissLoading) \_ -> do
-  platform <- liftEffect getPlatform
-  case platform of
-    Web -> void $ toAffE $ signInWithGoogle
-    IOS -> void $ toAffE $ signInWithGameCenter
-    Android -> void $ toAffE $ signInWithPlayGames
+signInFlow = bracket (toAffE $ presentLoading "Signing in...")
+  (toAffE <<< dismissLoading)
+  \_ ->
+    ( do
+        platform <- liftEffect getPlatform
+        case platform of
+          Web -> void $ toAffE $ signInWithGoogle
+          IOS -> void $ toAffE $ signInWithGameCenter
+          Android -> void $ toAffE $ signInWithPlayGames
+    ) <|> (alert "Login Failed" Nothing (Just "Please try again later") "OK")
 
 signOutFlow :: Aff Unit
 signOutFlow = toAffE signOut
