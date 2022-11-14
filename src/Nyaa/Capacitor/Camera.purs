d77b585e4d5229576f136192d256104d98bdc61d
Mike Solomon <mike120982@gmail.com>
2022-11-14 19:34:08 +0200

    Adds camera

---
+++ src/Nyaa/Capacitor/Camera.purs
@@ -0,0 +1,7 @@
+module Nyaa.Capacitor.Camera where
+
+import Control.Promise (Promise)
+import Data.ArrayBuffer.Types (Uint8Array)
+import Effect (Effect)
+
+foreign import takePicture :: Effect (Promise {webPath :: String, buffer :: Uint8Array})
