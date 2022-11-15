eebd16eda0f399585e0a78e88ea005a5c4ed308f
Mike Solomon <mike120982@gmail.com>
2022-11-15 14:56:04 +0200

    Adds more constants

---
+++ src/Nyaa/Constants/GameCenter.purs
@@ -0,0 +1,35 @@
+module Nyaa.Constants.GameCenter where
+
+
+track1LeaderboardID :: String
+track1LeaderboardID = "nyaa.track.1"
+track2LeaderboardID :: String
+track2LeaderboardID = "nyaa.track.2"
+track3LeaderboardID :: String
+track3LeaderboardID = "nyaa.track.3"
+tutorialAchievement :: String
+tutorialAchievement = "nyaa.tutorial"
+track1Achievement :: String
+track1Achievement = "nyaa.ac1"
+flatAchievement :: String
+flatAchievement = "nyaa.equalize"
+buzzAchievement :: String
+buzzAchievement = "nyaa.shift"
+glideAchievement :: String
+glideAchievement = "nyaa.glide"
+backAchievement :: String
+backAchievement = "nyaa.back"
+track2Achievement :: String
+track2Achievement = "nyaa.ac2"
+rotateAchievement :: String
+rotateAchievement = "nyaa.spin"
+hideAchievement :: String
+hideAchievement = "nyaa.hide"
+dazzleAchievement :: String
+dazzleAchievement = "nyaa.dazzle"
+track3Achievement :: String
+track3Achievement = "nyaa.ac3"
+crushAchievement :: String
+crushAchievement = "nyaa.crush"
+amplifyAchievement :: String
+amplifyAchievement = "nyaa.amplify"
