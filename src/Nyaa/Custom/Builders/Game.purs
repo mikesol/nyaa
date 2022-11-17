59340a8d2b4e81b534381cc331ca1a1dc34e74f8
Mike Solomon <mike120982@gmail.com>
2022-11-17 06:39:25 +0200

    Adds emoji activators

--- src/Nyaa/Custom/Builders/Game.purs
+++ src/Nyaa/Custom/Builders/Game.purs
@@ -7,6 +7,7 @@ import Data.Maybe (Maybe(..))
 import Deku.Attribute ((!:=))
 import Deku.Attributes (klass_, id_)
 import Deku.Control (text_)
+import Deku.Core (Nut)
 import Deku.DOM as D
 import Effect (Effect)
 import Effect.Aff (launchAff_)
@@ -24,6 +25,15 @@ import Web.HTML (HTMLCanvasElement, window)
 import Web.HTML.HTMLCanvasElement as HTMLCanvasElement
 import Web.HTML.HTMLDocument (toDocument)
 import Web.HTML.Window (document)
+
+fxButton :: { icon :: String, color :: String } -> Nut
+fxButton i = D.button
+  ( oneOf
+      [ klass_
+          $ i.color <> " font-semibold py-2 px-4 border border-gray-400 rounded shadow ml-2 mt-2"
+      ]
+  )
+  [ text_ i.icon ]
 
 foreign import startGame
   :: HTMLCanvasElement
@@ -46,7 +56,8 @@ game { name, audioContext, audioUri } = do
       liftEffect do
         w <- window
         d <- document w
-        c <- getElementById (name <> "-canvas") $ toNonElementParentNode $ toDocument d
+        c <- getElementById (name <> "-canvas") $ toNonElementParentNode $
+          toDocument d
         case c >>= HTMLCanvasElement.fromElement of
           Just canvas -> do
             controls <- startGame canvas "nyaa!" audioContext audioBuffer
@@ -59,22 +70,55 @@ game { name, audioContext, audioUri } = do
       v
   customComponent name {} gameStart gameEnd \_ ->
     [ ionContent (oneOf [ I.Fullscren !:= true ])
-        [ D.canvas (oneOf [ klass_ "absolute w-full h-full", id_ (name <> "-canvas") ])
+        [ D.canvas
+            (oneOf [ klass_ "absolute w-full h-full", id_ (name <> "-canvas") ])
             [
             ]
-        , D.div (oneOf [ klass_ "absolute w-full h-full grid grid-cols-3 grid-rows-3" ])
-            [ upperLeftBackButton
-            , D.div (oneOf [ klass_ "flex flex-col col-start-3 justify-self-end m-4" ])
-                [ D.span (oneOf [ id_ "score-player", klass_ "text-blue-500 text-2xl font-mono" ])
+        , D.div
+            ( oneOf
+                [ klass_ "absolute w-full h-full grid grid-cols-3 grid-rows-3" ]
+            )
+            [ D.div
+                ( oneOf
+                    [ klass_ "flex flex-col col-start-3 justify-self-end m-4" ]
+                )
+                [ D.span
+                    ( oneOf
+                        [ id_ "score-player"
+                        , klass_ "text-blue-500 text-2xl font-mono"
+                        ]
+                    )
                     [ text_ "0000000"
                     ]
-                , D.span (oneOf [ id_ "score-enemy", klass_ "text-green-500 text-2xl font-mono" ])
+                , D.span
+                    ( oneOf
+                        [ id_ "score-enemy"
+                        , klass_ "text-green-500 text-2xl font-mono"
+                        ]
+                    )
                     [ text_ "0000000"
                     ]
                 ]
-            , D.span (oneOf [ id_ "judgment", klass_ "text-white row-start-2 col-start-2 justify-self-center self-center text-2xl" ])
+            , D.span
+                ( oneOf
+                    [ id_ "judgment"
+                    , klass_
+                        "text-white row-start-2 col-start-2 justify-self-center self-center text-2xl"
+                    ]
+                )
                 [ text_ "..."
                 ]
+            ]
+        , D.div (klass_ "absolute")
+            [ fxButton { icon: "😬", color: "bg-red-200" } --
+            , fxButton { icon: "🎥", color: "bg-orange-100" } --
+            , fxButton { icon: "🚀", color: "bg-amber-100" } --
+            , fxButton { icon: "☝️", color: "bg-lime-100" } --
+            , fxButton { icon: "🌀", color: "bg-purple-100" } --
+            , fxButton { icon: "🙈", color: "bg-emerald-100" } --
+            , fxButton { icon: "✨", color: "bg-indigo-300" } --
+            , fxButton { icon: "🤘", color: "bg-rose-200" } --
+            , fxButton { icon: "📣", color: "bg-neutral-200" } --
             ]
         ]
     ]
